f0fa5f00f696c147dec277ff3680a4b573d3018d
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2024-07-02 09:19:24 +0200

    feat : added routes for client and auth

---
+++ routes/clientRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const clientController = require("../controllers/clientController");
+const {
+  authenticateToken,
+  authorizeAdminOrSelf,
+} = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router.get("/", authenticateToken, clientController.getAllClients);
+router.get(
+  "/:id",
+  authenticateToken,
+  authorizeAdminOrSelf,
+  clientController.getClientById
+);
+router.put(
+  "create/:id",
+  authenticateToken,
+  authorizeAdminOrSelf,
+  clientController.updateClient
+);
+router.delete(
+  "delete/:id",
+  authenticateToken,
+  authorizeAdminOrSelf,
+  clientController.deleteClient
+);
+
+module.exports = router;
