4976407f26b25e95da22e2e7eff59681f3dd9fd4
ivanh <i.hrybe@gmail.com>
2024-07-05 10:16:43 +0200

    feat: serverjs updated

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-<<<<<<< HEAD
-=======
 const path = require("path");
->>>>>>> 0f191c6a83c5498b203dfca77b990a8c2e67eeee
 const authRoutes = require("./routes/authRoutes");
 const clientRoutes = require("./routes/clientRoutes");
 const paintingRoutes = require("./routes/paintingRoutes");
@@ -24,11 +21,8 @@ app.use("/api/clients", clientRoutes);
 app.use("/api/paintings", paintingRoutes);
 app.use("/api/certificates", certificateRoutes);
 app.use("/api/orders", orderRoutes);
-<<<<<<< HEAD
 app.use('/public', express.static('public'));
-=======
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
->>>>>>> 0f191c6a83c5498b203dfca77b990a8c2e67eeee
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
