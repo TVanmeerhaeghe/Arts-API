38588d04542a46c729e5e26289f8ca00204babb2
ivanh <i.hrybe@gmail.com>
2024-07-05 10:19:11 +0200

    feat: routing updated

--- routes/paintingRoutes.js
+++ routes/paintingRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const paintingController = require("../controllers/paintingController");
-<<<<<<< HEAD
 const upload = require('../middlewares/upload');  // Assurez-vous que ce chemin est correct
-=======
->>>>>>> 0f191c6a83c5498b203dfca77b990a8c2e67eeee
 const {
   authenticateToken,
   authorizeAdminOrSelf,
@@ -18,20 +15,14 @@ router.post(
   "/create",
   authenticateToken,
   authorizeAdmin,
-<<<<<<< HEAD
   upload,
-=======
->>>>>>> 0f191c6a83c5498b203dfca77b990a8c2e67eeee
   paintingController.createPainting
 );
 router.put(
   "/update/:id",
   authenticateToken,
   authorizeAdmin,
-<<<<<<< HEAD
   upload,
-=======
->>>>>>> 0f191c6a83c5498b203dfca77b990a8c2e67eeee
   paintingController.updatePainting
 );
 router.delete(
